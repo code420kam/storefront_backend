64c4412798700ea36cc63932ffe891a1ec5d283d
K@mi1 <kamil.durhan.90@gmail.com>
2022-08-08 03:50:05 +0200

    bug fixed and adding test variables

--- src/db.ts
+++ src/db.ts
@@ -4,21 +4,21 @@ import dotenv from 'dotenv'
 dotenv.config()
 const { POSTGRES_DB, POSTGRES_PASSWORD, POSTGRES_URL, POSTGRES_USER, ENV, POSTGRES_TEST_DB } = process.env
 
-let client:Pool = new Pool;
-if(ENV === 'dev') {
+let client: Pool = new Pool()
+if (ENV === 'dev') {
     client = new Pool({
-      host: POSTGRES_URL,
-      database: POSTGRES_DB,
-      user: POSTGRES_USER,
-      password: POSTGRES_PASSWORD,
+        host: POSTGRES_URL,
+        database: POSTGRES_DB,
+        user: POSTGRES_USER,
+        password: POSTGRES_PASSWORD,
     })
-  }
-  if(ENV === 'test') {
-   client =  new Pool({
-      host: POSTGRES_URL,
-      database: POSTGRES_TEST_DB,
-      user: POSTGRES_USER,
-      password: POSTGRES_PASSWORD,
+}
+if (ENV === 'test') {
+    client = new Pool({
+        host: POSTGRES_URL,
+        database: POSTGRES_TEST_DB,
+        user: POSTGRES_USER,
+        password: POSTGRES_PASSWORD,
     })
-  }
-export default client;
+}
+export default client
--- src/order/controller.ts
+++ src/order/controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express'
 import OrderService from './service'
 
 export default class OrderCtrl {
-    static async getOrder(req: Request, res: Response) :Promise<Response|undefined>{
+    static async getOrder(req: Request, res: Response): Promise<Response | undefined> {
         const order = await OrderService.getCurrentOrder(req.params.id)
         if (order === null) {
             return res.status(404).send(`There are no active orders for User Id ${req.params.id}`)
